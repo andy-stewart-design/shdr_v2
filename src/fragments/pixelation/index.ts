986ae369ad21919f6e09f07a28e28d6b61923f4d
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-06-28 13:49:52 -0400

    pixelate demo

---
+++ src/fragments/pixelation/index.ts
@@ -0,0 +1,48 @@
+import { uniform, type FragmentFn } from "../../shdr/index.ts";
+
+export const uniforms = {
+  texture: uniform.texture2D("https://shdr.andystew.art/abstract.jpg"),
+  pixelation: uniform.float(40),
+};
+
+export const fragment: FragmentFn<typeof uniforms> = ({
+  $,
+  vec2,
+  floor,
+  step,
+  mix,
+  div,
+  texture,
+}) => {
+  const textureAR = $.let(
+    "textureAR",
+    $.u.textureResolution.x.div($.u.textureResolution.y),
+  );
+  const canvasAR = $.let("canvasAR", $.resolution.x.div($.resolution.y));
+
+  // Branchless object-fit: cover UV adjustment.
+  const useXScale = $.let("useXScale", step(canvasAR, textureAR));
+  const uvScale = $.let(
+    "uvScale",
+    mix(
+      vec2(1.0, textureAR.div(canvasAR)),
+      vec2(canvasAR.div(textureAR), 1.0),
+      useXScale,
+    ),
+  );
+  const adjustedUV = $.let("adjustedUV", $.uv.sub(0.5).mul(uvScale).add(0.5));
+
+  // Calculate square pixel grid in texture UV space.
+  const numPixelsY = $.let(
+    "numPixelsY",
+    floor($.resolution.y.div($.u.pixelation)),
+  );
+  const pixelSize = $.let("pixelSize", div(1.0, numPixelsY));
+  const dx = $.let("dx", pixelSize.div(textureAR));
+  const dy = $.let("dy", pixelSize);
+
+  const x = $.let("x", dx.mul(floor(adjustedUV.x.div(dx)).add(0.5)));
+  const y = $.let("y", dy.mul(floor(adjustedUV.y.div(dy)).add(0.5)));
+
+  $.output(texture($.u.texture, vec2(x, y)));
+};
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 import GUI from "lil-gui";
 import { addFloatUniformControl } from "./controls.ts";
 import { createShader, compileFragment } from "./shdr/index.ts";
-import { fragment, uniforms } from "./fragments/ben-day-spotlight";
+import { fragment, uniforms } from "./fragments/pixelation";
 
 console.log(compileFragment(fragment, { uniforms }));
 
@@ -11,4 +11,8 @@ const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
 createShader({ canvas, fragment, uniforms });
 
 const gui = new GUI();
-addFloatUniformControl(gui, "dpi", uniforms.dpi, { min: 2, max: 40, step: 1 });
+addFloatUniformControl(gui, "pixelation", uniforms.pixelation, {
+  min: 1,
+  max: 160,
+  step: 1,
+});
