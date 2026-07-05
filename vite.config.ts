aa0c526f224cc647b038bf0a5b02f5510c76799c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-07-05 13:08:30 -0400

    Add source import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import { shdrPlugin } from "./src/vite-plugin-shdr.ts";
 
 export default defineConfig({
   plugins: [shdrPlugin()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@shdr": fileURLToPath(new URL("./src/shdr", import.meta.url)),
+    },
+  },
 });
